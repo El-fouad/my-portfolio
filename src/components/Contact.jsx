97a2793d4fb618b550a887e952926e77162ce829
ELhamdaoui_Fouad <felhamd@gmail.com>
2023-07-26 19:28:16 +0100

    add contact component ' structure '

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-const Contact = () => {
-  return <div>Contact</div>;
+const  Contact = () => {
+  return <div className=' section' id='contact'>Contact</div>;
 };
 
 export default Contact;
